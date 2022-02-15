034e0ebc7d3debd45026b7e1a8d8bf950c87bdda
Pedro Loureiro <pedroql@google.com>
2022-02-15 16:11:07 +0000

    Add previously known codenames to UnboundedSdkLevel
    Test: atest com.android.modules.utils.build.UnboundedSdkLevelTest
    
    Merged-In: I6854f493dcb473ba277b23259e681a46da307f32
    Merged-In: I3bc90cb0f7a6c0b3f5edc6d3e89d5d42cf79df21
    (cherry picked from commit 89144ff5853ba1264ea5e07e73e5018151337232)
    
    Change-Id: I3bc90cb0f7a6c0b3f5edc6d3e89d5d42cf79df21

--- javatests/com/android/modules/utils/build/UnboundedSdkLevelTest.java
+++ javatests/com/android/modules/utils/build/UnboundedSdkLevelTest.java
@@ -20,6 +20,8 @@ import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assert.assertThrows;
 
+import android.util.SparseArray;
+
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -29,55 +31,26 @@ import java.util.Set;
 
 @RunWith(AndroidJUnit4.class)
 public class UnboundedSdkLevelTest {
+    private static final SparseArray<Set<String>> PREVIOUS_CODENAMES = new SparseArray<>();
+
+    static {
+        PREVIOUS_CODENAMES.put(29, Set.of("Q"));
+        PREVIOUS_CODENAMES.put(30, Set.of("Q", "R"));
+        PREVIOUS_CODENAMES.put(31, Set.of("Q", "R", "S"));
+        PREVIOUS_CODENAMES.put(32, Set.of("Q", "R", "S", "Sv2"));
+    }
 
     @Test
     public void testFinalizedSdk_S() {
         // Test against finalized S / 31 release
-        // Note empty set of known codenames, since on S builds Build.VERSION.KNOWN_CODENAMES
-        // does not exist. This is a realistic unit test reflecting S release builds.
-        UnboundedSdkLevel sdkLevel = new UnboundedSdkLevel(31, "REL", Set.of());
+        UnboundedSdkLevel sdkLevel = new UnboundedSdkLevel(31, "REL", PREVIOUS_CODENAMES.get(31));
 
         assertThat(sdkLevel.isAtLeastInternal("30")).isTrue();
         assertThat(sdkLevel.isAtLeastInternal("31")).isTrue();
         assertThat(sdkLevel.isAtLeastInternal("32")).isFalse();
 
-        assertThat(sdkLevel.isAtLeastInternal("R")).isFalse();
-        assertThat(sdkLevel.isAtLeastInternal("S")).isFalse();
-        assertThat(sdkLevel.isAtLeastInternal("Sv2")).isFalse();
-        assertThat(sdkLevel.isAtLeastInternal("Tiramisu")).isFalse();
-        assertThat(sdkLevel.isAtLeastInternal("U")).isFalse();
-
-        assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtLeastInternal(""));
-        assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtLeastInternal("current"));
-
-        assertThat(sdkLevel.isAtMostInternal("30")).isFalse();
-        assertThat(sdkLevel.isAtMostInternal("31")).isTrue();
-        assertThat(sdkLevel.isAtMostInternal("32")).isTrue();
-
-        assertThat(sdkLevel.isAtMostInternal("R")).isTrue();
-        assertThat(sdkLevel.isAtMostInternal("S")).isTrue();
-        assertThat(sdkLevel.isAtMostInternal("Sv2")).isTrue();
-        assertThat(sdkLevel.isAtMostInternal("Tiramisu")).isTrue();
-        assertThat(sdkLevel.isAtMostInternal("U")).isTrue();
-
-        assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtMostInternal(""));
-        assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtMostInternal("current"));
-    }
-
-    @Test
-    public void testFinalizedSdk() {
-        // Test against finalized S / 31 release
-        UnboundedSdkLevel sdkLevel = new UnboundedSdkLevel(31, "REL", Set.of("Q", "R", "S"));
-
-        assertThat(sdkLevel.isAtLeastInternal("30")).isTrue();
-        assertThat(sdkLevel.isAtLeastInternal("31")).isTrue();
-        assertThat(sdkLevel.isAtLeastInternal("32")).isFalse();
-
-        // R and S must have been re-compiled and changed to integer after SDK finalization
         assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtLeastInternal("R"));
         assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtLeastInternal("S"));
-
-        // Future versions are S+
         assertThat(sdkLevel.isAtLeastInternal("Sv2")).isFalse();
         assertThat(sdkLevel.isAtLeastInternal("Tiramisu")).isFalse();
         assertThat(sdkLevel.isAtLeastInternal("U")).isFalse();
@@ -162,6 +135,69 @@ public class UnboundedSdkLevelTest {
         assertThrows(IllegalArgumentException.class, () -> sdkLevel.isAtMostInternal("current"));
     }
 
+    @Test
+    public void testLegacySdkR() {
+        UnboundedSdkLevel sdkLevelR = new UnboundedSdkLevel(30, "REL",
+                PREVIOUS_CODENAMES.get(30));
+        assertThat(sdkLevelR.isAtLeastInternal("29")).isTrue();
+        assertThat(sdkLevelR.isAtLeastInternal("30")).isTrue();
+        assertThat(sdkLevelR.isAtLeastInternal("31")).isFalse();
 
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelR.isAtLeastInternal("Q")));
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelR.isAtLeastInternal("R")));
+        assertThat(sdkLevelR.isAtLeastInternal("S")).isFalse();
+        assertThat(sdkLevelR.isAtLeastInternal("Sv2")).isFalse();
+        assertThat(sdkLevelR.isAtLeastInternal("Tiramisu")).isFalse();
+
+        assertThat(sdkLevelR.isAtMostInternal("29")).isFalse();
+        assertThat(sdkLevelR.isAtMostInternal("30")).isTrue();
+        assertThat(sdkLevelR.isAtMostInternal("31")).isTrue();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelR.isAtMostInternal("Q")));
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelR.isAtMostInternal("R")));
+        assertThat(sdkLevelR.isAtMostInternal("S")).isTrue();
+        assertThat(sdkLevelR.isAtMostInternal("Sv2")).isTrue();
+        assertThat(sdkLevelR.isAtMostInternal("Tiramisu")).isTrue();
+
+        assertThrows(IllegalArgumentException.class, () -> sdkLevelR.isAtMostInternal(""));
+        assertThrows(IllegalArgumentException.class, () -> sdkLevelR.isAtMostInternal("current"));
+    }
+
+    @Test
+    public void testLegacySdkQ() {
+        UnboundedSdkLevel sdkLevelQ = new UnboundedSdkLevel(29, "REL",
+                PREVIOUS_CODENAMES.get(29));
+
+        assertThat(sdkLevelQ.isAtLeastInternal("28")).isTrue();
+        assertThat(sdkLevelQ.isAtLeastInternal("29")).isTrue();
+        assertThat(sdkLevelQ.isAtLeastInternal("30")).isFalse();
+        assertThat(sdkLevelQ.isAtLeastInternal("31")).isFalse();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelQ.isAtLeastInternal("Q")));
+        assertThat(sdkLevelQ.isAtLeastInternal("R")).isFalse();
+        assertThat(sdkLevelQ.isAtLeastInternal("S")).isFalse();
+        assertThat(sdkLevelQ.isAtLeastInternal("Sv2")).isFalse();
+        assertThat(sdkLevelQ.isAtLeastInternal("Tiramisu")).isFalse();
+
+        assertThat(sdkLevelQ.isAtMostInternal("28")).isFalse();
+        assertThat(sdkLevelQ.isAtMostInternal("29")).isTrue();
+        assertThat(sdkLevelQ.isAtMostInternal("30")).isTrue();
+        assertThat(sdkLevelQ.isAtMostInternal("31")).isTrue();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> assertThat(sdkLevelQ.isAtMostInternal("Q")));
+        assertThat(sdkLevelQ.isAtMostInternal("R")).isTrue();
+        assertThat(sdkLevelQ.isAtMostInternal("S")).isTrue();
+        assertThat(sdkLevelQ.isAtMostInternal("Sv2")).isTrue();
+        assertThat(sdkLevelQ.isAtMostInternal("Tiramisu")).isTrue();
+
+        assertThrows(IllegalArgumentException.class, () -> sdkLevelQ.isAtMostInternal(""));
+        assertThrows(IllegalArgumentException.class, () -> sdkLevelQ.isAtMostInternal("current"));
+    }
 
 }
